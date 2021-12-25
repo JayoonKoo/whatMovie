e0c5279273050ce5cf76c060cc34678cab68dc5c
JayoonKoo <rnwkdbs12@gmail.com>
2021-12-26 02:21:18 +0900

    feat: MoviesScreen 헤더 보이게 설정

--- screens/RootStack.tsx
+++ screens/RootStack.tsx
@@ -1,19 +1,15 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import MovieScreen from './MovieScreen';
 import MoviesScreen from './MoviesScreen';
+import MovieScreen from './MovieScreen';
 import {RootStackParamList} from './types';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const RootStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Main"
-        options={{headerShown: false}}
-        component={MoviesScreen}
-      />
+    <Stack.Navigator screenOptions={{headerBackTitle: ''}}>
+      <Stack.Screen name="Main" component={MoviesScreen} />
       <Stack.Screen
         name="Movie"
         options={{title: 'Movie'}}
