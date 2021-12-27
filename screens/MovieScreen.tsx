565a60039bdfac9d20824409a3e10bc040c120dd
JayoonKoo <rnwkdbs12@gmail.com>
2021-12-27 14:33:02 +0900

    feat: MovieScreen 추가

--- screens/MovieScreen.tsx
+++ screens/MovieScreen.tsx
@@ -1,12 +1,43 @@
+import {RouteProp, useRoute} from '@react-navigation/native';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {ActivityIndicator, StyleSheet} from 'react-native';
+import {useQuery} from 'react-query';
+import {getMovie} from '../api/movies';
+import Empty from '../components/common/Empty';
+import MovieDetail from '../components/MovieDetail';
+import {RootStackParamList} from './types';
+
+type MovieScreenRouteProp = RouteProp<RootStackParamList, 'Movie'>;
 
 const MovieScreen = () => {
-  return (
-    <View>
-      <Text> MovieScreen </Text>
-    </View>
+  const {
+    params: {movieId},
+  } = useRoute<MovieScreenRouteProp>();
+
+  const {data, isLoading, error} = useQuery(['movie', movieId], () =>
+    getMovie(movieId),
   );
+
+  if (isLoading) {
+    return (
+      <ActivityIndicator style={styles.spinner} size="small" color="gray" />
+    );
+  }
+
+  if (error) {
+    return <Empty text={error as string} />;
+  }
+
+  return <>{data && <MovieDetail movie={data} />}</>;
 };
+
+const styles = StyleSheet.create({
+  spinner: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F7A630',
+  },
+});
 
 export default MovieScreen;
